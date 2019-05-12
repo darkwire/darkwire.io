7d298a42a4e39e64f9dec06fd2d10611e38ce1e3
Alan Friedman <d.alan.friedman@gmail.com>
2019-05-12 16:34:11 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ yarn  --production=false
 REACT_APP_API_HOST=$API_HOST \
 REACT_APP_API_PROTOCOL=$API_PROTOCOL \
 REACT_APP_API_PORT=$API_PORT \
-yarn react-scripts build
+yarn build
 cd ../
 
 echo "building server..."
