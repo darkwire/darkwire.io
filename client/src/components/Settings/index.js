6b1bc068452b9281f014407a78d7864c70e866d1
Alan Friedman <d.alan.friedman@gmail.com>
2019-08-24 07:43:22 -0400

    Update translations help link

--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -38,7 +38,7 @@ class Settings extends Component {
 
         <section>
           <h4 className='mb-3'><T path='languageDropdownHeader'/></h4>
-          <p><a href="https://github.com/darkwire/darkwire.io/tree/master/client/src/i18n" target="_blank">Help us translate Darkwire!</a></p>
+          <p><a href="https://github.com/darkwire/darkwire.io/blob/master/client/README.md#translations" target="_blank">Help us translate Darkwire!</a></p>
           <div className="form-group">
             <select value={this.props.language} className="form-control" onChange={this.handleLanguageChange.bind(this)}>
               <option value="en">English</option>
