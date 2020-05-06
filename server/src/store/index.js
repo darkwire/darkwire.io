b3a28aa0e579850a4968df1ba6ed77a46e9607a5
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-05-06 21:07:45 +0200

    Store is now lazy loaded

--- server/src/store/index.js
+++ server/src/store/index.js
@@ -1,20 +1,25 @@
 import MemoryStore from './Memory';
 import RedisStore from './Redis';
 
-const storeBackend = process.env.STORE_BACKEND || 'redis';
-const storeHost = process.env.STORE_HOST || process.env.REDIS_URL;
-
 let store;
-switch (storeBackend) {
-  case 'memory':
-    store = new MemoryStore();
-    break;
-  case 'redis':
-  default:
-    store = new RedisStore(storeHost);
-    break;
-}
 
-const getStore = () => store;
+const getStore = () => {
+  // Load store on first demand
+  if(store === undefined){
+    const storeBackend = process.env.STORE_BACKEND || 'redis';
+    const storeHost = process.env.STORE_HOST || process.env.REDIS_URL;
+    
+    switch (storeBackend) {
+      case 'memory':
+        store = new MemoryStore();
+        break;
+      case 'redis':
+      default:
+        store = new RedisStore(storeHost);
+        break;
+    }
+  }
+  return store;
+};
 
 export default getStore;
