fbccc98a51bacb8f894042601ae5ededae10494b
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-05-23 23:03:57 +0200

    Better coverage for Home component

--- client/src/components/Home/Home.test.js
+++ client/src/components/Home/Home.test.js
@@ -1,66 +1,72 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Home from './Home';
+import { Provider } from 'react-redux';
+import configureStore from 'store';
+
+const store = configureStore();
 
 jest.mock('react-modal'); // Cant load modal without root app element
-jest.mock('components/T'); // Need store
-jest.mock('components/Chat'); // Need store
-jest.mock('utils/socket', () => { // Avoid exception
+jest.mock('utils/socket', () => {
+  // Avoid exception
   return {
-    connect: jest.fn().mockImplementation(()=>{
+    connect: jest.fn().mockImplementation(() => {
       return {
         on: jest.fn(),
-        emit: jest.fn()
-      }
-    })
-  }; 
+        emit: jest.fn(),
+      };
+    }),
+  };
 }); //
 
-jest.mock('utils/crypto', () => { // Need window.crytpo.subtle
+jest.mock('utils/crypto', () => {
+  // Need window.crytpo.subtle
   return jest.fn().mockImplementation(() => {
     return {
       createEncryptDecryptKeys: () => {
         return {
           privateKey: 'private',
-          publicKey: 'public'
-        }
+          publicKey: 'public',
+        };
       },
       exportKey: () => {
-        return 'exportedkey'
-      }
-    }
-  }); 
+        return 'exportedkey';
+      },
+    };
+  });
 });
 
 test('Home component is displaying', async () => {
   const { asFragment } = render(
-    <Home
-      translations={{}}
-      members={[]}
-      openModal={()=>{}}
-      activities={[]}
-      match={{params:{roomId: "roomTest"}}}
-      createUser={()=>{}}
-      toggleSocketConnected={()=>{}}
-      receiveEncryptedMessage={()=>{}}
-      receiveUnencryptedMessage={()=>{}}
-      scrolledToBottom={true}
-      setScrolledToBottom={()=>{}}
-      iAmOwner={true}
-      roomLocked={false}
-      userId={'userId'}
-      roomId={'testId'}
-      sendEncryptedMessage={()=>{}}
-      sendUnencryptedMessage={()=>{}}
-      socketConnected={false}
-      toggleSoundEnabled={()=>{}}
-      soundIsEnabled={false}
-      faviconCount={0}
-      toggleWindowFocus={()=>{}}
-      closeModal={()=>{}}
-      publicKey={{}}
-      username={'linus'}
-    />
+    <Provider store={store}>
+      <Home
+        translations={{}}
+        members={[]}
+        openModal={() => {}}
+        activities={[]}
+        match={{ params: { roomId: 'roomTest' } }}
+        createUser={() => {}}
+        toggleSocketConnected={() => {}}
+        receiveEncryptedMessage={() => {}}
+        receiveUnencryptedMessage={() => {}}
+        scrolledToBottom={true}
+        setScrolledToBottom={() => {}}
+        iAmOwner={true}
+        roomLocked={false}
+        userId={'userId'}
+        roomId={'testId'}
+        sendEncryptedMessage={() => {}}
+        sendUnencryptedMessage={() => {}}
+        socketConnected={false}
+        toggleSoundEnabled={() => {}}
+        soundIsEnabled={false}
+        faviconCount={0}
+        toggleWindowFocus={() => {}}
+        closeModal={() => {}}
+        publicKey={{}}
+        username={'linus'}
+      />
+    </Provider>,
   );
 
   expect(asFragment()).toMatchSnapshot();
