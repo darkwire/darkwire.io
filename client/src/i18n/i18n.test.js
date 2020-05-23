ab447190afecae2be11dabcca6b4a72a5d3e4ad9
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-05-23 20:52:25 +0200

    100% coverage for translations

--- client/src/i18n/i18n.test.js
+++ client/src/i18n/i18n.test.js
@@ -1,9 +1,10 @@
-import { getTranslations } from './index.js'
+import { getTranslations } from './index.js';
 
 test('Get translation', () => {
-  expect(getTranslations('en').welcomeHeader).toBe("Welcome to Darkwire v2.0");
-  expect(getTranslations('fr').welcomeHeader).toBe("Bienvenue sur Darkwire v2.0");
-  expect(getTranslations('zh-CN').welcomeHeader).toBe("欢迎来到Darkwire v2.0");
-  expect(getTranslations('en-US').welcomeHeader).toBe("Welcome to Darkwire v2.0");
-  expect(getTranslations('ru-CH').welcomeHeader).toBe("Welcome to Darkwire v2.0");
-})
+  expect(getTranslations('en').welcomeHeader).toBe('Welcome to Darkwire v2.0');
+  expect(getTranslations().welcomeHeader).toBe('Welcome to Darkwire v2.0');
+  expect(getTranslations('fr').welcomeHeader).toBe('Bienvenue sur Darkwire v2.0');
+  expect(getTranslations('zh-CN').welcomeHeader).toBe('欢迎来到Darkwire v2.0');
+  expect(getTranslations('en-US').welcomeHeader).toBe('Welcome to Darkwire v2.0');
+  expect(getTranslations('ru-CH').welcomeHeader).toBe('Welcome to Darkwire v2.0');
+});
