16be9dea8349d076ea67783ab25ca261f45b7a2d
Alan Friedman <d.alan.friedman@gmail.com>
2020-06-11 08:43:51 -0400

    Revert "Rework Home component phase 2" (#152)
    This reverts commit db6831603123dfdaf763bdf2f0fae85f044b99a9.

--- client/src/components/Home/ActivityList.js
+++ client/src/components/Home/ActivityList.js
@@ -1,90 +1,100 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ChatInput from 'components/Chat'
-import Activity from './Activity'
-import T from 'components/T'
 import { defer } from 'lodash'
+import Activity from './Activity'
+
+import T from 'components/T'
 
 import styles from './styles.module.scss'
 
-const ActivityList = ({ activities, openModal }) => {
-  const [focusChat, setFocusChat] = React.useState(false);
-  const [scrolledToBottom, setScrolledToBottom] = React.useState(true);
-  const messageStream = React.useRef(null);
-  const activitiesList = React.useRef(null);
+class ActivityList extends Component {
+  constructor(props) {
+    super(props)
 
-  React.useEffect(() => {
-    const currentMessageStream = messageStream.current;
-
-    // Update scrolledToBottom state if we scroll the activity stream
-    const onScroll = () => {
-      const messageStreamHeight = messageStream.current.clientHeight
-      const activitiesListHeight = activitiesList.current.clientHeight
-
-      const bodyRect = document.body.getBoundingClientRect()
-      const elemRect = activitiesList.current.getBoundingClientRect()
-      const offset = elemRect.top - bodyRect.top
-      const activitiesListYPos = offset
-
-      const newScrolledToBottom = (activitiesListHeight + (activitiesListYPos - 60)) <= messageStreamHeight
-      if (newScrolledToBottom) {
-        if (!scrolledToBottom) {
-          setScrolledToBottom(true)
-        }
-      } else if (scrolledToBottom) {
-        setScrolledToBottom(false)
-      }
+    this.state = {
+      zoomableImages: [],
+      focusChat: false,
     }
-
-    currentMessageStream.addEventListener('scroll', onScroll)
-    return () => {
-      // Unbind event if component unmounted
-      currentMessageStream.removeEventListener('scroll', onScroll)
-    }
-  }, [scrolledToBottom])
-
-  const scrollToBottomIfShould = React.useCallback(() => {
-    if (scrolledToBottom) {
-      messageStream.current.scrollTop = messageStream.current.scrollHeight
-    }
-  }, [scrolledToBottom])
-
-  React.useEffect(() => {
-    scrollToBottomIfShould(); // Only if activities.length bigger
-  }, [scrollToBottomIfShould, activities]);
-
-  const scrollToBottom = React.useCallback(() => {
-    messageStream.current.scrollTop = messageStream.current.scrollHeight
-    setScrolledToBottom(true)
-  }, [])
-
-  const handleChatClick = () => {
-    setFocusChat(true);
-    defer(() => setFocusChat(false))
   }
 
-  return (
-    <div className="main-chat">
-      <div onClick={handleChatClick} className="message-stream h-100" ref={messageStream} data-testid="main-div">
-        <ul className="plain" ref={activitiesList}>
-          <li><p className={styles.tos}><button className='btn btn-link' onClick={() => openModal('About')}> <T path='agreement'/></button></p></li>
-          {activities.map((activity, index) => (
+  componentDidMount() {
+    this.bindEvents()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.activities.length < this.props.activities.length) {
+      this.scrollToBottomIfShould()
+    }
+  }
+
+  onScroll() {
+    const messageStreamHeight = this.messageStream.clientHeight
+    const activitiesListHeight = this.activitiesList.clientHeight
+
+    const bodyRect = document.body.getBoundingClientRect()
+    const elemRect = this.activitiesList.getBoundingClientRect()
+    const offset = elemRect.top - bodyRect.top
+    const activitiesListYPos = offset
+
+    const scrolledToBottom = (activitiesListHeight + (activitiesListYPos - 60)) <= messageStreamHeight
+    if (scrolledToBottom) {
+      if (!this.props.scrolledToBottom) {
+        this.props.setScrolledToBottom(true)
+      }
+    } else if (this.props.scrolledToBottom) {
+      this.props.setScrolledToBottom(false)
+    }
+  }
+
+  scrollToBottomIfShould() {
+    if (this.props.scrolledToBottom) {
+      setTimeout(() => {
+        this.messageStream.scrollTop = this.messageStream.scrollHeight
+      }, 0)
+    }
+  }
+
+  scrollToBottom() {
+    this.messageStream.scrollTop = this.messageStream.scrollHeight
+    this.props.setScrolledToBottom(true)
+  }
+
+  bindEvents() {
+    this.messageStream.addEventListener('scroll', this.onScroll.bind(this))
+  }
+
+  handleChatClick() {
+    this.setState({ focusChat: true })
+    defer(() => this.setState({ focusChat: false }))
+  }
+
+  render() {
+    return (
+      <div className="main-chat">
+        <div onClick={this.handleChatClick.bind(this)} className="message-stream h-100" ref={el => this.messageStream = el} data-testid="main-div">
+          <ul className="plain" ref={el => this.activitiesList = el}>
+            <li><p className={styles.tos}><button className='btn btn-link' onClick={this.props.openModal.bind(this, 'About')}> <T path='agreement'/></button></p></li>
+            {this.props.activities.map((activity, index) => (
             <li key={index} className={`activity-item ${activity.type}`}>
-              <Activity activity={activity} scrollToBottom={scrollToBottomIfShould} />
+                <Activity activity={activity} scrollToBottom={this.scrollToBottomIfShould.bind(this)} />
             </li>
-          ))}
-        </ul>
+            ))}
+          </ul>
+          </div>
+          <div className="chat-container">
+          <ChatInput scrollToBottom={this.scrollToBottom.bind(this)} focusChat={this.state.focusChat} />
+        </div>
       </div>
-      <div className="chat-container">
-        <ChatInput scrollToBottom={scrollToBottom} focusChat={focusChat} />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 ActivityList.propTypes = {
   activities: PropTypes.array.isRequired,
   openModal: PropTypes.func.isRequired,
+  setScrolledToBottom: PropTypes.func.isRequired,
+  scrolledToBottom: PropTypes.bool.isRequired,
 }
 
-export default ActivityList
+export default ActivityList;
