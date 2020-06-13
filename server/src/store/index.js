3f17ca707bbc957b3251becfe4cb568d4efbfd31
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-06-13 15:52:27 +0200

    Add lint command

--- server/src/store/index.js
+++ server/src/store/index.js
@@ -5,10 +5,10 @@ let store;
 
 const getStore = () => {
   // Load store on first demand
-  if(store === undefined){
+  if (store === undefined) {
     const storeBackend = process.env.STORE_BACKEND || 'redis';
     const storeHost = process.env.STORE_HOST || process.env.REDIS_URL;
-    
+
     switch (storeBackend) {
       case 'memory':
         store = new MemoryStore();
