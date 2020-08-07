80ea577ba6ad11290dca04312f47401cabe5d740
yaBobJonez <yaBobJonez@gmail.com>
2020-08-07 12:50:33 +0300

    Russian translation proposal
    I'm a Russian native speaker from Ukraine. Note:
    "(-a)" is used as the standard ending in Russian to indicate female gender-specific ending.

--- client/src/i18n/i18n.test.js
+++ client/src/i18n/i18n.test.js
@@ -6,5 +6,5 @@ test('Get translation', () => {
   expect(getTranslations('fr').welcomeHeader).toBe('Bienvenue sur Darkwire v2.0');
   expect(getTranslations('zh-CN').welcomeHeader).toBe('欢迎来到Darkwire v2.0');
   expect(getTranslations('en-US').welcomeHeader).toBe('Welcome to Darkwire v2.0');
-  expect(getTranslations('ru-CH').welcomeHeader).toBe('Welcome to Darkwire v2.0');
+  expect(getTranslations('ru-CH').welcomeHeader).toBe('Добро пожаловать на Darkwire v2.0');
 });
