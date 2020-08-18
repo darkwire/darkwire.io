72419acff38231da38ea878267bf2dcec691cda5
yaBobJonez <yaBobJonez@gmail.com>
2020-08-18 21:21:14 +0300

    Russian translation expectation testing
    Am I supposed to change this one?

--- client/src/i18n/i18n.test.js
+++ client/src/i18n/i18n.test.js
@@ -7,4 +7,5 @@ test('Get translation', () => {
   expect(getTranslations('zh-CN').welcomeHeader).toBe('欢迎来到Darkwire v2.0');
   expect(getTranslations('en-US').welcomeHeader).toBe('Welcome to Darkwire v2.0');
   expect(getTranslations('ru-CH').welcomeHeader).toBe('Welcome to Darkwire v2.0');
+  expect(getTranslations('ru').welcomeHeader).toBe('Добро пожаловать на Darkwire v2.0');
 });
